a2804309a28b291662dd407a0e48d20e615feed2
Paul Paquin <paulepaquin@gmail.com>
2018-02-15 20:28:39 -0600

    Add files via upload

--- src/robot.py
+++ src/robot.py
@@ -5,6 +5,7 @@ Main code for Robot
 
 import wpilib
 import robotmap
+import ctre
 from wpilib import Joystick
 from subsystems.drivetrain import DriveTrain as Drive
 from subsystems.grabber import Grabber
@@ -25,9 +26,6 @@ class Robot(wpilib.IterativeRobot):
         self.cStick = Joystick(self.kSticks['control'])
         self.drive = Drive(self)
         self.cubeGrabber = Grabber(self)
-        
-        # holds data from the FMS about the field elements.
-        self.fielddata = None
 
 
     def robotPeriodic(self):
@@ -43,37 +41,29 @@ class Robot(wpilib.IterativeRobot):
         """
         This function is run once each time the robot enters autonomous mode.
         """
-        # get field data -- currently removed until we add an IF statement
-        # self.fielddata = wpilib.DriverStation.getInstance().getGameSpecificMessage()
-        
-        self.drive.driveLeftMaster.setQuadraturePosition(0, 0)
-        self.drive.driveRightMaster.setQuadraturePosition(0, 0) 
+        # get field data
+        #self.fielddata = wpilib.DriverStation.getInstance().getGameSpecificMessage()        
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-        
-        ## get field data
-        #if not self.fielddata:
-            #self.fielddata = wpilib.DriverStation.getInstance().getGameSpecificMessage()          
-            #nearswitch, scale, farswitch = list(self.fielddata)
-        
-        #if self.fielddata[0] == 'R':
-            #self.drive.arcade(.5, .2)
-        #else:
-            #self.drive.arcade(.5, -.2)
-            
-        self.drive.arcade(.5, 0)
+        #nearswitch, scale, farswitch = list(self.fielddata)
+#         
+#         if nearswitch == 'R':
+#             self.drive.arcade(.5, .2)
+#         else:
+#             self.drive.arcade(.5, -.2)
+        pass
 
     def teleopInit(self):
-        self.drive.driveLeftMaster.setQuadraturePosition(0, 0)
-        self.drive.driveRightMaster.setQuadraturePosition(0, 0)
+        pass
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         speed = self.dStick.getY() * -1
         rotation = self.dStick.getTwist()
         # self.drive.moveSpeed(speed, speed)
-        self.drive.arcade(speed, rotation)
+        
+        self.drive.arcadeWithRPM(speed, rotation, 2800)
         
         self.cubeGrabber.grabberFunction()
 
@@ -81,6 +71,7 @@ class Robot(wpilib.IterativeRobot):
         pass
 
     def testPeriodic(self):
+        wpilib.LiveWindow.setEnabled(True)
         pass
 
 
