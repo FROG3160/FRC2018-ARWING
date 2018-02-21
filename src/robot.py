9091ad6a437e2aaa63bd305693db0ea1ac0082b8
Paul Paquin <paulepaquin@gmail.com>
2018-02-20 22:02:49 -0600

    Add files via upload

--- src/robot.py
+++ src/robot.py
@@ -7,10 +7,11 @@ import wpilib
 import robotmap
 from wpilib import Joystick
 from subsystems.drivetrain import DriveTrain as Drive
-from subsystems.grabber import Grabber
+from subsystems.grabber import cubeGrabber
 from subsystems.elevator import Elevator
 from subsystems.climber import Climber
 from subsystems.autonomous import Autonomous
+# from robotpy_ext.common_drivers.navx import AHRS
 
 
 class Robot(wpilib.IterativeRobot):
@@ -30,11 +31,13 @@ class Robot(wpilib.IterativeRobot):
         self.cStick = Joystick(self.kSticks['control'])
         
         self.drive = Drive(self)
-        self.cubeGrabber = Grabber(self)
+        self.cubeGrabber = cubeGrabber(self)
         self.elevator = Elevator(self)
         self.climber = Climber(self)
         
-        self.autonomous = Autonomous(self)
+        
+        
+    
 
 
     def robotPeriodic(self):
@@ -48,27 +51,34 @@ class Robot(wpilib.IterativeRobot):
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
+        self.autonomous = Autonomous(self)
         self.autonomous.getGameData()
+        self.autonomous.reset()
+        self.drive.autoInit()
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-        self.autonomous.run()
-
+        #self.autonomous.run()
+        #self.autonomous.testMove(self.autonomous.magEncoderInchesToTicks(12*6), -1, True)
+        self.autonomous.testAngle(90, -1)
+        self.autonomous.telemetry()
+        
     def teleopInit(self):
-        pass
+        self.drive.teleInit()
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         speed = self.dStick.getY() * -1
         rotation = self.dStick.getTwist()
         # self.drive.moveSpeed(speed, speed)
-        
+         
         self.drive.arcadeWithRPM(speed, rotation, 2800)
-        
+          
         self.cubeGrabber.grabberFunction()
-        
+#          
         self.elevator.elevatorFunction()
-        
+        self.elevator.telemetry()
+          
         self.climber.climberFunction()
 
     def testInit(self):
@@ -77,6 +87,7 @@ class Robot(wpilib.IterativeRobot):
     def testPeriodic(self):
         wpilib.LiveWindow.setEnabled(True)
         pass
+    
 
 
 if __name__ == "__main__":
